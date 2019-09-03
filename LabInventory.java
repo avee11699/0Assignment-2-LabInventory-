34fc8d1ba7f59f4ba4f321f1a079829a0ef6e8c7
Avneesh C Chuttoor <52330723+avee11699@users.noreply.github.com>
2019-09-03 20:06:21 +0400

    Update LabInventory.java
    the program is not ready.... so don't use it

--- LabInventory.java
+++ LabInventory.java
@@ -56,13 +56,84 @@ public class LabInventory {
 						}
 					}
 				}
-				System.out.print("How many items you want to enter?");
 				
+			        System.out.print("How many items you want to enter?");
+				qty = myinput.nextInt();
+				
+				for(int i =0; i<=qty; i++) {
+					System.out.print("Item ID: ");
+					ItemID = myinput.next();
+					
+					System.out.print("Name: ");
+					ItemName = myinput.next();
+					
+					System.out.print("Description: ");
+					Description = myinput.next();
+					
+					System.out.print("Category: ");
+					category = myinput.next();
+					
+					System.out.print("Supplier name: ");
+					supplierName = myinput.next();
+					
+					System.out.print("Supplier contact: ");
+					SupplierContact = myinput.nextInt();
+					
+					System.out.print("Price: ");
+					price = myinput.nextInt();
+					
+					System.out.print("Available qty: ");
+					availableQuantity = myinput.nextInt();
+					
+					itemDatabase[i]= new Item(ItemID, ItemName, Description, category, supplierName, SupplierContact, price, availableQuantity);
+					itemDatabase[i].setItemid(ItemID);
+					itemDatabase[i].setItemname(ItemName);
+					itemDatabase[i].setDescription(Description);
+					itemDatabase[i].setCategory(category);
+					itemDatabase[i].setSupplierName(supplierName);
+					itemDatabase[i].setSupplierContact(SupplierContact);
+					itemDatabase[i].setAvailableQty(availableQuantity);
+				}
 			}
 			if(choice == 2) {
-				System.out.println("which item you wish to update?");
-				update = myinput.next();
+				System.out.println("What infromation?");
+				System.out.println("	1. Name");
+				System.out.println("	2. Descriptiom");
+				System.out.println("	3. Category");
+				System.out.println("	4. Supplier name");
+				System.out.println("	5. Supplier contacts");
+				System.out.println("	6. Price");
+				System.out.println("	7. Available qty");
+				System.out.println("	8. Quit");
+				System.out.println("Please enter your choice > ");
+				update = myinput.nextInt();
 			}
+			
+			if(choice == 3) {
+				System.out.print("");
+			}
+			
+			if(choice == 4) {
+				for(int i= 0; i<=qty; i++) {
+					
+					itemDatabase[i].getItemid();
+					itemDatabase[i].getItemname();
+					itemDatabase[i].getSupplierName();
+					itemDatabase[i].getSupplierContact();
+					
+					System.out.print(itemDatabase[i].toString());
+				
+				} 
+				
+			}
+			
+			if(choice == 5) {
+				
+			}
+			
+			if(choice == 6) {
+				System.out.print("Thank you for using the program.");
+			break;	
 		
 		}
 	}
