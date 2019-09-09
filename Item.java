de01ae5722b4d92d4c40082724c13ec98f751426
Avneesh C Chuttoor <52330723+avee11699@users.noreply.github.com>
2019-09-09 21:40:31 +0400

    Add files via upload

--- Item.java
+++ Item.java
@@ -6,115 +6,181 @@
  * @author avneesh
  *
  */
-public abstract class Item {
-	//defining attributes
-	private String name;
-	private String description;
-	private String category;
-	private String SupplierName;
-	private String Suppliercontact;
-	private double price;
-	private int AvailableQty;
-	/**
+public class Item {
+	//defining attributes 
+		private String itemid;
+		private String itemname;
+		private String Description;
+		private double price;
+		private int availableQty;
+		private String Category;
+		private String supplierName;
+		private int SupplierContact;
+		private String availablequantity;
+		static int count;
+	/**p
 	 * 
 	 */
 	public Item() {
-		super();
+		// TODO Auto-generated constructor stub
+		this.itemid = null;
+		this.itemname = null;
+		this.Description = null;
+		this.Category = null;
+		this.supplierName= null;
+		this.SupplierContact = 0;
+		this.price = 0.0;
+		this.availableQty = 0;
 	}
-	public Item(String name, String description, String category, String SupplierName, String Suppliercontact, double price, int AvailableQty) {
-			
-			this.name = name;
-			this.description = description;
-			this.category = category;
-			this.SupplierName = SupplierName;
-			this.Suppliercontact = Suppliercontact;
-			this.price = price;
-			this.AvailableQty = AvailableQty;
+	
+	public Item(String itemid, String itemname, String Description, String Category, String SupplierName, int SupplierContact,double price, int availableQty) {
+		this.itemid = itemid;
+		this.itemname = itemname;
+		this.Description = Description;
+		this.Category = Category;
+		this.supplierName= SupplierName;
+		this.SupplierContact = SupplierContact;
+		this.price = price;
+		this.availableQty = availableQty;
+		count++;
 	}
+
+	
 	/**
-	 * @return the name
+	 * @return the itemid
 	 */
-	public String getName() {
-		return name;
+	public String getItemid() {
+		return itemid;
 	}
+
+	/**
+	 * @return the itemname
+	 */
+	public String getItemname() {
+		return itemname;
+	}
+
 	/**
 	 * @return the description
-	 */
+	 */ 
 	public String getDescription() {
-		return description;
+		return Description;
 	}
-	/**
-	 * @return the category
-	 */
-	public String getCategory() {
-		return category;
-	}
-	/**
-	 * @return the supplierName
-	 */
-	public String getSupplierName() {
-		return SupplierName;
-	}
-	/**
-	 * @return the suppliercontact
-	 */
-	public String getSuppliercontact() {
-		return Suppliercontact;
-	}
+
 	/**
 	 * @return the price
 	 */
 	public double getPrice() {
 		return price;
 	}
+
 	/**
 	 * @return the availableQty
 	 */
 	public int getAvailableQty() {
-		return AvailableQty;
+		return availableQty;
 	}
+
 	/**
-	 * @param name the name to set
+	 * @return the category
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public String getCategory() {
+		return Category;
 	}
+
+	/**
+	 * @return the supplierName
+	 */
+	public String getSupplierName() {
+		return supplierName;
+	}
+
+	/**
+	 * @return the supplierContact
+	 */
+	public int getSupplierContact() {
+		return SupplierContact;
+	}
+
+	/**
+	 * @param itemid the itemid to set
+	 */
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	/**
+	 * @param itemname the itemname to set
+	 */
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+
 	/**
 	 * @param description the description to set
 	 */
 	public void setDescription(String description) {
-		this.description = description;
+		Description = description;
 	}
-	/**
-	 * @param category the category to set
-	 */
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	/**
-	 * @param supplierName the supplierName to set
-	 */
-	public void setSupplierName(String supplierName) {
-		SupplierName = supplierName;
-	}
-	/**
-	 * @param suppliercontact the suppliercontact to set
-	 */
-	public void setSuppliercontact(String suppliercontact) {
-		Suppliercontact = suppliercontact;
-	}
+
 	/**
 	 * @param price the price to set
 	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	/**
 	 * @param availableQty the availableQty to set
 	 */
 	public void setAvailableQty(int availableQty) {
-		AvailableQty = availableQty;
+		this.availableQty = availableQty;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(String category) {
+		Category = category;
+	}
+
+	/**
+	 * @param supplierName the supplierName to set
+	 */
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+
+	/**
+	 * @param supplierContact the supplierContact to set
+	 */
+	public void setSupplierContact(int supplierContact) {
+		SupplierContact = supplierContact;
+	}
+
+	/**
+	 * compare if two items have the same id
+	 * @param i
+	 * @return true if both items' id are similar
+	 */
+	public boolean equals(Item i) {
+		if(this.itemid.equalsIgnoreCase(i.itemid)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
+	public static void totalitem(){
+		System.out.print("Number of items currently in store: " + count);
+	}
 	
+	@Override
+	public String toString() {
+
+	
+		 return ("Item ID = " + this.itemid + ", Item name=" + this.itemname + ", Description = " + this.Description + ", Category = " + this.Category +
+		  		   ",Supplier name = " + this.supplierName + ", Supplier Contact = " + this.SupplierContact + ", Price =  " + this.price + ", Available Quantity = "+ this.availablequantity) ; 
+	}
 }
