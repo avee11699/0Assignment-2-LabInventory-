649579b96ba4ca0c70c655aebe38b4e99f0c8142
Avneesh C Chuttoor <52330723+avee11699@users.noreply.github.com>
2019-09-09 21:39:39 +0400

    Add files via upload

--- LabInventory.java
+++ LabInventory.java
@@ -18,17 +18,25 @@ public class LabInventory {
 		Scanner myinput = new Scanner(System.in);
 		int count = 1;
 		String password = "DitProg123";
-		String update = null;
+		int update;
 		int fail = 0;
-		
+		String ItemID;
+		String ItemName;
+		String Description;
+		String category; 
+		String supplierName;
+		int SupplierContact;
+		int price;
+		int availableQuantity;
+		int size = 70;
+		Item[]itemDatabase = new Item[size];
+		int qty = 0;
+		boolean x = true; 
 		/**
 		 * display menu 
 		 */
-		
-		if(count == 1){
 			
-			System.out.print("What do you want to do?");
-			System.out.println(" ");
+			System.out.println("What do you want to do?");
 			System.out.println("	1. Enter a new item to inventory(password required)");
 			System.out.println("	2. Change information of an existing item(password required)");
 			System.out.println("	3. Display all the items with the specifies category");
@@ -40,63 +48,62 @@ public class LabInventory {
 			int choice = myinput.nextInt();
 			
 			if(choice == 1) {
-				System.out.print("enter password: ");
-				password = myinput.next();
-				
-				while (password != "DitProg123"){
-					while (fail < 3 ) {
-				
-						System.out.print("Wrong PASSWORD! Please enter correct PASSWORD:");
-						password = myinput.next();
+				for(fail = 0; fail <= 3; fail++) {
+					System.out.print("enter password: ");
+					password = myinput.next();
+					
+					if(password.equalsIgnoreCase(password)) {
 						
-						fail = fail + 1;
+						System.out.print("How many items you want to enter?");
+						qty = myinput.nextInt();
+						Item[] itemdatabase = new Item[size];
 						
-						if(fail == 4) {
+						for(int i =0; i<=qty; i++) {
+							System.out.print("Item ID: ");
+							ItemID = myinput.next();
+							
+							System.out.print("Name: ");
+							ItemName = myinput.next();
+							
+							System.out.print("Description: ");
+							Description = myinput.next();
+							
+							System.out.print("Category: ");
+							category = myinput.next();
+							
+							System.out.print("Supplier name: ");
+							supplierName = myinput.next();
+							
+							System.out.print("Supplier contact: ");
+							SupplierContact = myinput.nextInt();
+							
+							System.out.print("Price: ");
+							price = myinput.nextInt();
+							
+							System.out.print("Available qty: ");
+							availableQuantity = myinput.nextInt();
+							
+							
+							itemDatabase[i]= new Item(ItemID, ItemName, Description, category, supplierName, SupplierContact, price, availableQuantity);
+							itemDatabase[i].setItemid(ItemID);
+							itemDatabase[i].setItemname(ItemName);
+							itemDatabase[i].setDescription(Description);
+							itemDatabase[i].setCategory(category);
+							itemDatabase[i].setSupplierName(supplierName);
+							itemDatabase[i].setSupplierContact(SupplierContact);
+							itemDatabase[i].setAvailableQty(availableQuantity);
+						}
+					}
+					else {
+						if (fail ==4) {
 							System.out.print("Program detected suspicious activity and is terminating!");
+							break;
 						}
 					}
 				}
-				
-			        System.out.print("How many items you want to enter?");
-				qty = myinput.nextInt();
-				
-				for(int i =0; i<=qty; i++) {
-					System.out.print("Item ID: ");
-					ItemID = myinput.next();
-					
-					System.out.print("Name: ");
-					ItemName = myinput.next();
-					
-					System.out.print("Description: ");
-					Description = myinput.next();
-					
-					System.out.print("Category: ");
-					category = myinput.next();
-					
-					System.out.print("Supplier name: ");
-					supplierName = myinput.next();
-					
-					System.out.print("Supplier contact: ");
-					SupplierContact = myinput.nextInt();
-					
-					System.out.print("Price: ");
-					price = myinput.nextInt();
-					
-					System.out.print("Available qty: ");
-					availableQuantity = myinput.nextInt();
-					
-					itemDatabase[i]= new Item(ItemID, ItemName, Description, category, supplierName, SupplierContact, price, availableQuantity);
-					itemDatabase[i].setItemid(ItemID);
-					itemDatabase[i].setItemname(ItemName);
-					itemDatabase[i].setDescription(Description);
-					itemDatabase[i].setCategory(category);
-					itemDatabase[i].setSupplierName(supplierName);
-					itemDatabase[i].setSupplierContact(SupplierContact);
-					itemDatabase[i].setAvailableQty(availableQuantity);
-				}
 			}
 			if(choice == 2) {
-				System.out.println("What infromation?");
+				System.out.println("What infromation of the item you wish to update?");
 				System.out.println("	1. Name");
 				System.out.println("	2. Descriptiom");
 				System.out.println("	3. Category");
@@ -107,10 +114,12 @@ public class LabInventory {
 				System.out.println("	8. Quit");
 				System.out.println("Please enter your choice > ");
 				update = myinput.nextInt();
+				
+				
 			}
 			
 			if(choice == 3) {
-				System.out.print("");
+				System.out.print("hgjkyhb");
 			}
 			
 			if(choice == 4) {
@@ -128,14 +137,16 @@ public class LabInventory {
 			}
 			
 			if(choice == 5) {
-				
+				Item.totalitem();
+				System.out.print(" ");
 			}
 			
 			if(choice == 6) {
 				System.out.print("Thank you for using the program.");
-			break;	
-		
+			System.exit(0);	
+			}
 		}
 	}
-}
+
+
 
